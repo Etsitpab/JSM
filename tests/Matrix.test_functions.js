0750b7b93db74fc84583eb15b89867ea259ecfea
Baptiste Mazin <baptiste.mazin@gmail.com>
2015-03-04 07:54:27 +0100

    Add fourier tests and minor corrections.

--- tests/Matrix.test_functions.js
+++ tests/Matrix.test_functions.js
@@ -237,4 +237,63 @@
         }
     };
 
+    Matrix._benchmarkWavelets = function (N, name, dim) {
+        N = N || 100;
+	name = name || 'haar';
+	dim = dim || 1;
+        
+        var s, wt, out, time, psnr;
+        var SQN = Math.round(Math.sqrt(N)); 
+	s = Matrix.ones(SQN, SQN, SQN, SQN * 3).cumsum(dim)["-"](1); 
+        Tools.tic();
+	wt = Matrix.dwt(s, name, dim);
+	out = Matrix.idwt(wt, name, dim);
+	time = Tools.toc()
+	psnr = Matrix.psnr(s, out).getDataScalar().toFixed(2) + "dB";
+        console.log("DWT 1D decomposotion/recomposition", "PSNR:", psnr, "Time:", time);
+
+	s = Matrix.ones(N, N, 3).cumsum(0)["-"](1);
+	Tools.tic();
+	wt = Matrix.dwt2(s, name);
+	out = Matrix.idwt2(wt, name);
+        time = Tools.toc()
+	psnr = Matrix.psnr(s, out).getDataScalar().toFixed(2) + "dB";
+        console.log("DWT 2D decomposotion/recomposition", "PSNR:", psnr, "Time:", time);
+
+	Tools.tic();
+	wt = Matrix.dwt(s, name, 0);
+	var wt1 = Matrix.dwt(wt[0], name, 1);
+	var wt2 = Matrix.dwt(wt[1], name, 1);
+	var iwt1 = Matrix.idwt(wt1, name, 1);
+	var iwt2 = Matrix.idwt(wt2, name, 1);
+	out = Matrix.idwt([iwt1, iwt2], name, 0);
+        time = Tools.toc()
+	psnr = Matrix.psnr(s, out).getDataScalar().toFixed(2) + "dB";
+        console.log("DWT 2D decomposotion/recomposition from DWT 1D", "PSNR:", psnr, "Time:", time);
+    };
+
+    Matrix._benchmarkFourier = function (N) {
+        N = N || 5;
+        
+        var s, fft, out, time, psnr;
+        var SQN = Math.round(Math.sqrt(N)); 
+	s = Matrix.randi(9, N, 1);//ones(N * N, 1).cumsum()["-"](1);
+        s = Matrix.toMatrix([7, 0, 2, 1, 4]);
+        Tools.tic();
+	fft = Matrix.fft(s);
+	out = Matrix.ifft(fft);
+	time = Tools.toc()
+	psnr = s["-"](out)[".^"](2).abs().mean().getDataScalar().toFixed(2);
+        console.log("FFT 1D decomposotion/recomposition", "L2:", psnr, "Time:", time);
+
+	s = Matrix.randi(9, N, N);
+	Tools.tic();
+	fft = Matrix.fft2(s);
+	out = Matrix.ifft2(fft);
+        time = Tools.toc()
+	psnr = s["-"](out)[".^"](2).mean().getDataScalar().toFixed(2);
+        console.log("FFT 2D decomposotion/recomposition", "PSNR:", psnr, "Time:", time);
+
+    };
+
 })(Matrix, Matrix.prototype);
