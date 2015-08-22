32eca4bf183cccf697542cc76b56c340534ac826
Baptiste Mazin <baptiste.mazin@gmail.com>
2015-08-23 01:38:16 +0200

    Update demo to SuperCanvas

--- examples/pipe/demo.js
+++ examples/pipe/demo.js
@@ -1,35 +1,25 @@
 /*global console, document, Matrix, Colorspaces, CIE, open */
 /*jshint indent: 4, unused: true, white: true */
 
-var IMAGE;
-var $ = function (id) {
-    return document.getElementById(id);
-};
+var IMAGE, RAW, MEASURES, SC;
 
 function displayImage(image) {
     'use strict';
-
-    // image = limitImageSize(image, MAX_SIZE);
-    // imgOrig = image.im2double();
-    // imgCurrent = image.im2double();
-
-    
     var imagePlot = $('imagePlot').getPlot().clear();
+    Tools.tic();
     image.toImage(function () {
         imagePlot.addImage(this, 0, 0, {id: 'workingImage'});
-    });
+        console.log("Image displayed in", Tools.toc(), "ms");
+    }, 'image/jpeg', 0.7);
+    drawImageHistogram("histogram", image);
 }
 
 function updateOutput(image) {
     "use strict";
-    var outputCanvas = $("outputImage");
-    var div = $("image");
-    var canvasXSize = div.offsetWidth;
-    var canvasYSize = div.offsetHeight;
-    outputCanvas.width = canvasXSize;
-    outputCanvas.height = canvasYSize;
-    image.imshow(outputCanvas, "fit");
-    outputCanvas.style.marginTop = (div.offsetHeight - outputCanvas.height) / 2;
+    Tools.tic();
+    SC.setImage(image);
+    console.log("Image displayed in", Tools.toc(), "ms");
+    // drawImageHistogram("histogram", image);
 }
 
 var readRAW = function (RAW) {
@@ -46,7 +36,7 @@ var readRAW = function (RAW) {
     var infos = new Uint32Array(RAW, 20, 6);
     var padding = tab2char(new Uint32Array(RAW, 44, 20));
     var types = [
-        null, null,
+        'uint16', 'int16',
         'uint8', 'int8',
         'uint16', 'int16', 'uint32', 'int32',
         null, null, null, null, null,
@@ -61,7 +51,7 @@ var readRAW = function (RAW) {
     console.log("width:", w);
     console.log("height:", h);
     console.log("prec:", prec);
-    console.log("type:", type);
+    console.log("type:", type, infos[3]);
     console.log("nChannels:", c);
     console.log(padding);
     var constructor = Tools.checkType(type);
@@ -81,7 +71,6 @@ var readFile = function (file, callback, type) {
         callback = callback.bind(evt.target.result);
         callback(evt);
     };
-    console.log(type);
     switch (type) {
     case 'dataurl':
     case 'url':
@@ -171,6 +160,134 @@ Matrix.prototype.blackPoint = function (Grm, Rm, Bm, Gbm, max) {
     return this;
 };
 
+Matrix.prototype.applySRGBGamma = function (resolution) {
+    var lut = new Float64Array(resolution);
+    var a = 0.055, I2D4 = 1 / 2.4, v, ires = 1 / resolution;
+    for (var i = 0; i < resolution; i++) {
+        v = i * ires;
+        lut[i] = (v > 0.0031308) ? (1.055 * Math.pow(v, I2D4) - a) : (v * 12.92);
+    }
+    var data = this.getData(), dLength = data.length;
+    for (var i = 0; i < dLength; i++) {
+        v = Math.floor(data[i] * resolution);
+        if (v > 0 && v < resolution) {
+            data[i] = lut[v];
+        }
+    }
+    return this;
+};
+
+var parameters = {
+    'bp': 200,
+    'awbScales': [1.8, 1.5],
+    'colorMatrix': [1.82421875, -0.10546875, 0.009765625, -0.978515625, 1.283203125, -0.658203125, 0.154296875, -0.17578125, 1.6484375]
+    // 'colorMatrix': [2.52773308,  0.25255637, 0.456801887, -1.187522717, 1.282750378, -1.717001308, 0.531574993,  0.29519138, 3.1736645]
+    //'colorMatrix': [1.50040757, -1.09400685, -0.821907892, 2.4726743960199724, 5.5861898074911025, 3.980233823833764, -1.6973025709652312, -1.7503258349248325, -0.7896378620413816]
+    // 'colorMatrix': [2.873079181990579, -2.259509600516003, -1.4947969746811658, 3.8773990971906374, 9.980523004501183, 6.644431507344027, -3.8417120823170383, -3.851354503867207, -2.0165997425226587]
+    // 'colorMatrix': [2.6188478147662946, -0.3490482043425507, 0.6491755198871962, -0.6053859282024999, 2.6462704898824474, -3.2805723249583476, 0.5764082596919458, 0.5250529842158762, 5.330607239358602]
+    // 'colorMatrix':[2.4845042444757817, -0.4321723102360087, 0.6278390282791709, -0.6056796601790841, 2.6198328547568783, -3.331207796322422, 0.5048008807512255, 0.39041046281402697, 5.08720600257353]
+    //'colorMatrix':[1.8822833041294111, -0.757891263314495, -0.30761510659279917, 1.4334357463628882, 4.283124157908033, 2.4312801936179476, -0.8423291580634884, -0.9042177794394357, 0.006550670018218689]
+};
+
+var changeParameters = function () {
+    parameters.bp = $F("bpVal");
+    parameters.awbScales = Matrix.dlmread($V("awbScalesVal")).getData();
+};
+
+var initParameters = function () {
+    $V("bpVal", parameters.bp);
+    $V("awbScalesVal", parameters.awbScales[0].toFixed(4) + ", " + parameters.awbScales[1].toFixed(4));
+    $("bpVal").addEventListener("change", changeParameters);
+    $("awbScalesVal").addEventListener("change", changeParameters);
+    $("scaleBVal").addEventListener("change", changeParameters);
+    $("develop").addEventListener("click", develop);
+};
+
+var processRaw = function (RAW) {
+    Tools.tic();
+    var bp = parameters.bp, max = RAW.max().getDataScalar() - bp;
+    IMAGE = RAW.blackPoint(bp, bp, bp, bp, max);
+    console.log("Black point removed in", Tools.toc(), "ms");
+    
+    Tools.tic();
+    // IMAGE = IMAGE.demosaicTrivial();
+    IMAGE = IMAGE.demosaic();
+    console.log("RAW demosaiced in", Tools.toc(), "ms");
+    
+    // Tools.tic();
+    // IMAGE = IMAGE.sqrt().imbilateral(3, 0.0001, 3).power(2);
+    // console.log("Bilateral denoising applied in", Tools.toc(), "ms");
+    
+    // Tools.tic();
+    // IMAGE = IMAGE.sqrt().wdenoise(0.025, 'sym4').power(2);
+    // console.log("Wavelet denoising applied in", Tools.toc(), "ms");
+    
+    Tools.tic();
+    var WB = Matrix.toMatrix([
+        parameters.awbScales[0], 0.00,                    0.00,
+        0.00,                    1.00,                    0.00,
+        0.00,                    0.00, parameters.awbScales[1]
+    ]).reshape(3, 3);
+    var CM = Matrix.toMatrix(parameters.colorMatrix).reshape(3, 3);
+    IMAGE.applycform(CM.mtimes(WB));
+    console.log("Color Matrix applied in", Tools.toc(), "ms");
+    
+    
+    Tools.tic();
+    IMAGE.applySRGBGamma(4096);
+    console.log("sRGB tone curve applied in", Tools.toc(), "ms");
+    
+    // Tools.tic();
+    // IMAGE = IMAGE.colorEnhancement(0.5, 15, 10, "sym4", 0.1)
+    // console.log("Wavelet enhancement applied in", Tools.toc(), "ms");
+    
+    return IMAGE;
+};
+
+var develop = function () {
+    Tools.tic();
+    IMAGE = processRaw(RAW.getCopy());
+    console.log("Image processed in", Tools.toc(), "ms");
+    updateOutput(IMAGE);
+    // displayImage(IMAGE);
+};
+
+var wbTuningFromMeasures = function () {
+    var p01 = MEASURES[0].scales,
+        p02 = MEASURES[1].scales,
+        p03 = MEASURES[2].scales,
+        p04 = MEASURES[3].scales,
+        p04 = MEASURES[3].scales,
+        p15 = MEASURES[14].scales,
+        p16 = MEASURES[15].scales,
+        p21 = MEASURES[20].scales,
+        p22 = MEASURES[21].scales;
+
+    var gretag = []
+    var concat = function(v, i, t) {
+        gretag = gretag.concat(Array.prototype.slice.call(t[i].mean));
+    };
+    MEASURES.forEach(concat);
+    return {
+        grey: [
+            0.5 * p21[0] + 0.5 * p22[0],
+            0.5 * p21[1] + 0.5 * p22[1]
+        ],
+        skin: [
+            0.5 * p01[0] + 0.5 * p02[0],
+            0.5 * p01[1] + 0.5 * p02[1]
+        ],
+        green: [
+            0.09 * p03[0] + 0.74 * p04[0] + 0.12 * p16[0] + 0.05 * p21[0],
+            0.09 * p03[1] + 0.74 * p04[1] + 0.12 * p16[1] + 0.05 * p21[1]
+        ],
+        blue: [
+            0.59 * p03[0] + 0.06 * p04[0] + 0.06 * p15[0] + 0.29 * p21[0],
+            0.59 * p03[1] + 0.06 * p04[1] + 0.06 * p15[1] + 0.29 * p21[1]
+        ],
+        gretag: gretag
+    }   
+};
 
 window.onload = function () {
     "use strict";
@@ -191,57 +308,47 @@ window.onload = function () {
             'preserve-ratio': true
         };
         window.imagePlot = new Plot('imagePlot', [$('image').clientWidth, $('image').clientHeight], 'image', plotProperties);
+        imagePlot.selectarea = function (x1, y1, x2, y2) {
+            var m = Math.min, M = Math.max, r = Math.round;
+            var Y1 = r(M(m(-y1, -y2), 0) / 2);
+            var Y2 = r(m(M(-y1, -y2), IMAGE.getSize(0) - 1) / 2);
+            var X1 = r(M(m(x1, x2), 0) / 2);
+            var X2 = r(m(M(x2, x2), IMAGE.getSize(1) - 1) / 2);
+            var patch = RAW.get([Y1, Y2], [X1, X2]);
+            patch.reshape([patch.size(0) * patch.size(1), patch.size(2)]);
+            var pMean = patch.mean(0),
+                pStd = patch.std(0),
+                pMin = patch.min(0),
+                pMax = patch.max(0);
+            var wp = pMean['-'](parameters.bp).getData();
+            var G = (wp[0] + wp[
+3]) * 0.5;
+            var scales = [G / wp[1], G / wp[2]];
+            MEASURES.push({
+                "patch": patch,
+                "mean": pMean.getData(),
+                "std": pStd.getData(),
+                "min": pMin.getData(),
+                "max": pMax.getData(),
+                "scales": scales
+            });
+            if ($V("Shift+click") === "wb") {
+                parameters.awbScales = scales;
+                $V("awbScalesVal", parameters.awbScales[0].toFixed(4) + ", " + parameters.awbScales[1].toFixed(4));
+                develop();            
+            }
+        };
+        imagePlot.click = function () {
+        };
     };
-    createPlot();
+    // createPlot();
     var read = function (evt) {
         var callback = function (evt) {
             Tools.tic();
-            IMAGE = readRAW(this).double();
-            console.log("RAW of size", IMAGE.size(), "read in", Tools.toc(), "ms");
-            Tools.tic();
-            {
-                Tools.tic();
-                var bp = 135, max = IMAGE.max().getDataScalar() - bp;
-                IMAGE.blackPoint(bp, bp, bp, bp, max);
-                console.log("Black point removed in", Tools.toc(), "ms");
-                
-                Tools.tic();
-                // IMAGE = IMAGE.demosaicTrivial();
-                IMAGE = IMAGE.demosaic();
-                console.log("RAW demosaiced in", Tools.toc(), "ms");
-                
-                // Tools.tic();
-                // IMAGE = IMAGE.sqrt().imbilateral(3, 0.0001, 3).power(2);
-                // console.log("Bilateral denoising applied in", Tools.toc(), "ms");
-                
-                // Tools.tic();
-                // IMAGE = IMAGE.sqrt().wdenoise(0.025, 'sym4').power(2);
-                // console.log("Wavelet denoising applied in", Tools.toc(), "ms");
-                
-                Tools.tic();
-                var WB = Matrix.toMatrix([
-                    1.50, 0.00, 0.00,
-                    0.00, 1.00, 0.00,
-                    0.00, 0.00, 1.50
-                ]).reshape(3, 3);
-                var CM = Matrix.toMatrix([
-                    1.993689, -1.152317,  0.158628,
-                   -0.151371,  1.359525, -0.208153,
-                   -0.023086, -0.796688,  1.819774
-                ]).reshape(3, 3).transpose();
-                IMAGE.applycform(WB);
-                // IMAGE.applycform(WB.mtimes(CM));
-                console.log("Color Matrix applied in", Tools.toc(), "ms");
-                
-                
-                Tools.tic();
-                // IMAGE.applycform('LinearRGB to sRGB');
-                // IMAGE = IMAGE['.^'](0.42);
-                IMAGE = IMAGE.sqrt();
-                console.log("sRGB tone curve applied in", Tools.toc(), "ms");
-            }
-            console.log("Image processed in", Tools.toc(), "ms");
-            displayImage(IMAGE);
+            RAW = readRAW(this).double();
+            console.log("RAW of size", RAW.size(), "read in", Tools.toc(), "ms");
+            MEASURES = [];
+            develop();
         };
 
         // Only call the handler if 1 or more files was dropped.
@@ -255,5 +362,13 @@ window.onload = function () {
     };
     $("loadFile").addEventListener("change", read, false);
 
-    hideFieldset();
+    var outputCanvas = $("outputImage");
+    SC = new SuperCanvas(outputCanvas);
+    var div = $("image");
+    var canvasXSize = div.offsetWidth;
+    var canvasYSize = div.offsetHeight;
+    outputCanvas.width = canvasXSize;
+    outputCanvas.height = canvasYSize;
+    initParameters();
+    // hideFieldset();
 };
